43bc41bab691f31d829a9197dd00581e81be6d83
Adam Voss <vossad01@gmail.com>
2017-07-11 16:05:03 -0500

    Miscellaneous cleanup

--- src/test/anyjson.test.ts
+++ src/test/anyjson.test.ts
@@ -9,28 +9,29 @@ import * as chai from 'chai'
 const assert = chai.assert;
 
 suite('encode', function () {
-
+  suite('product-set', () => {
     const input = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
 
-    for (const format of Object.keys(anyjson.Formats).filter(n => typeof(anyjson.Formats[n]) === 'number')) {
-        test(format, async function () {
-            const actual = anyjson.encode(input, format)
-            const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
-            return assert.strictEqual(await actual, await expected)
-        })
+    for (const format of Object.keys(anyjson.Formats).filter(n => typeof (anyjson.Formats[n]) === 'number')) {
+      test(format, async function () {
+        const actual = anyjson.encode(input, format)
+        const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
+        return assert.strictEqual(await actual, await expected)
+      })
     }
-})
+  });
+});
 
 suite('decode', function () {
-
+  suite('product-set', () => {
     const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
 
-    for (const format of Object.keys(anyjson.Formats).filter(n => typeof(anyjson.Formats[n]) === 'number')) {
-        test(format, async function () {
-            const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
-            const actual = await anyjson.decode(format, contents);
-            return assert.deepEqual(actual, expected)
-        })
+    for (const format of Object.keys(anyjson.Formats).filter(n => typeof (anyjson.Formats[n]) === 'number')) {
+      test(format, async function () {
+        const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
+        const actual = await anyjson.decode(format, contents);
+        return assert.deepEqual(actual, expected)
+      })
     }
-
-})
+  });
+});
