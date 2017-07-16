d8e44c9455b34150c771767b462292e95b34e16d
Adam Voss <vossad01@gmail.com>
2017-07-15 20:33:59 -0500

    Update README and package information

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 # Usage
 
+// TODO: Update command line interface
+
 ```
 usage: any-json [OPTIONS] [INPUT]
 
@@ -30,10 +32,38 @@ usage: any-json [OPTIONS] [INPUT]
 npm install any-json -g
 ```
 
-# API
+# Data Safety
 
-See lib/anyjson.js
+## Safe
+
+When only JSON features are used, conversion should not result in any data loss when using these formats.
+
+- cson
+- hjson
+- json
+- json5
+- yaml
+
+## Problematic
+
+Some loss of information may occur.  Improved parsers/serializers could provide better compatibility, but implementation is provided as-is.  Known issues are listed below the format.
+
+- ini
+  - All numbers are converted to strings (the `ini` library would need to quote strings in order be recognize numbers)
+  - In the unlikely case an object was not an array but has only has sequential, numeric members starting at zero it will be decoded as an array.
+- xml
+  - It cannot parse its own output (node-xml2js#391)[https://github.com/Leonidas-from-XIV/node-xml2js/issues/391]
+
+## Limited
+
+These formats are conceptually different and only work on a limited basis.  With effort, conventions could be established to provide a more complete transfer but there will be some impedance.
+
+Tabular formats:
+- csv
+- xls
+- xlsx
 
 # History
 
+- v3 Re-written to be Promise-based and bi-directional (serialization capabilities as well as parsing).
 - v2 removed the experimental/unreliable format detection.
--- src/test/anyjson.test.ts
+++ src/test/anyjson.test.ts
@@ -9,43 +9,114 @@ import * as chai from 'chai'
 
 const assert = chai.assert;
 
-const formats = [
-  "cson",
-  "csv",
-  "hjson",
-  "ini",
-  "json",
-  "json5",
-  "xls",
-  "xlsx",
-  "xml",
-  "yaml"
-]
+suite('safe-formats', () => {
 
-suite('encode', function () {
-  suite('product-set', () => {
-    const input = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+  const safe_formats = [
+    "cson",
+    "hjson",
+    "json",
+    "json5",
+    "yaml"
+  ]
 
-    for (const format of formats) {
-      test(format, async function () {
-        const actual = anyjson.encode(input, format)
-        const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
-        return assert.strictEqual(await actual, await expected)
-      })
-    }
+  suite('encode', function () {
+    suite('product-set', () => {
+      const input = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const actual = anyjson.encode(input, format)
+          const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
+          return assert.strictEqual(await actual, await expected)
+        })
+      }
+    });
+  });
+
+  suite('decode', function () {
+    suite('product-set', () => {
+      const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
+          const actual = await anyjson.decode(format, contents);
+          return assert.deepEqual(actual, expected)
+        })
+      }
+    });
   });
 });
 
-suite('decode', function () {
-  suite('product-set', () => {
-    const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+suite('problematic-formats', () => {
 
-    for (const format of formats) {
-      test(format, async function () {
-        const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
-        const actual = await anyjson.decode(format, contents);
-        return assert.deepEqual(actual, expected)
-      })
-    }
+  const safe_formats = [
+    "ini",
+    "xml"
+  ]
+
+  suite('encode', function () {
+    suite('product-set', () => {
+      const input = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const actual = anyjson.encode(input, format)
+          const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
+          return assert.strictEqual(await actual, await expected)
+        })
+      }
+    });
+  });
+
+  suite('decode', function () {
+    suite('product-set', () => {
+      const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
+          const actual = await anyjson.decode(format, contents);
+          return assert.deepEqual(actual, expected)
+        })
+      }
+    });
+  });
+});
+
+suite('tabular-formats', () => {
+
+  const safe_formats = [
+    "csv",
+    "xls",
+    "xlsx"
+  ]
+
+  suite('encode', function () {
+    suite('product-set', () => {
+      const input = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const actual = anyjson.encode(input, format)
+          const expected = readFile(path.join(__dirname, 'fixtures', 'out', `product-set.${format}`), 'utf8')
+          return assert.strictEqual(await actual, await expected)
+        })
+      }
+    });
+  });
+
+  suite('decode', function () {
+    suite('product-set', () => {
+      const expected = JSON.parse(fs.readFileSync(path.join(__dirname, 'fixtures', 'in', 'product-set.json'), 'utf8'));
+
+      for (const format of safe_formats) {
+        test(format, async function () {
+          const contents = fs.readFileSync(path.join(__dirname, 'fixtures', 'out', 'product-set.' + format), 'utf8')
+          const actual = await anyjson.decode(format, contents);
+          return assert.deepEqual(actual, expected)
+        })
+      }
+    });
   });
 });
